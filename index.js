273bba6f79329c833dfb52922f4a9b87bef5b44e
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2019-02-25 12:17:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 "use strict";
 
-module.exports = function () {
-  var callback = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : function () {};
-  var name = arguments.length > 1 ? arguments[1] : undefined;
+module.exports = {
+  swat: function swat() {
+    var callback = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : function () {};
+    var name = arguments.length > 1 ? arguments[1] : undefined;
+    watch(callback(), name || callback);
+  },
+  swatEval: function swatEval() {
+    var code = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "";
+    var name = arguments.length > 1 ? arguments[1] : undefined;
+    watch(eval(code), name);
+  }
+};
+
+var watch = function watch(doit, name) {
   try {
-    var _name = n === undefined ? name.toString() : name;
+    var n = name === undefined ? doit.toString() : name;
     console.time(n);
-    var r = callback();
+    var r = doit;
     console.timeEnd(n);
     console.log("RETURN: ".concat(r));
   } catch (e) {
